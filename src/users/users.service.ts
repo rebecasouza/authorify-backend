1391074b7628919bdd7e47244f6f7898af47cc39
Rebeca Souza <rebecasouza@empresavila.com.br>
2023-01-23 23:07:32 -0300

    ✨ Adds user signup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  create(name: string, email: string) {
+    const user = this.repository.create({ name, email });
+
+    return this.repository.save(user);
+  }
+}
